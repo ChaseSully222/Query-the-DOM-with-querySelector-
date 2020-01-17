a2d47b7249ad3f38623fd9d74cb62ed4c9b7fcf8
Chase Sullins <jcsullins222@yahoo.com>
2020-01-16 22:27:01 -0600

    removed the dashed border, added goldenrod background color to footer, exercise completed

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const articleHeader= document.querySelector(".article__header");
+const articleHeader = document.querySelector(".article__header");
 articleHeader.textContent = "Welcome to the Chase blog";
 
+const articleHeaderClass = document.querySelectorAll(".article__header");
+articleHeaderClass.classlist = "article__header important";
+
+const dashedEl = document.querySelector(".dashed");
+dashedEl.classList.remove("dashed")
+
+const articleFooterEl = document.querySelector(".article__footer");
+articleFooterEl.classList.add("goldenrod");
 
